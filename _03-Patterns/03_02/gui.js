ed73d4b1520935e014113c623ce1c768ec72ef80
Jony Margelist <jony.margelist@stud.hslu.ch>
2017-11-30 16:05:17 +0100

    added several new layers and and ui

---
+++ _03-Patterns/03_02/gui.js
@@ -0,0 +1,60 @@
+var options = {
+  arcX: 0,
+  arcY: 0,
+  arcStartRadian: 90,
+  arcEndRadian: 180,
+
+  lineX: 90,
+  lineY: 0,
+
+  pointCounts: 5,
+  pointStrokeWeight: 10,
+
+  quadX: 0,
+  quadY: 0,
+
+  ellipseX: 0,
+  ellipseY: 0,
+
+  triangleX2: 0,
+  triangleY2: 0,
+
+  rectX:0,
+  rectY:0,
+};
+
+window.onload = function() {
+  var gui = new dat.GUI();
+
+  var myArc = gui.addFolder('arc');
+  myArc.add(options, 'arcX').min(0).max(360).step(1);
+  myArc.add(options, 'arcY').min(0).max(360).step(1);
+  myArc.add(options, 'arcStartRadian').min(0).max(3.6).step(.1);
+  myArc.add(options, 'arcEndRadian').min(0).max(3.6).step(.1);
+
+  var myLine = gui.addFolder('line');
+  myLine.add(options, 'lineX').min(0).max(360).step(1);
+  myLine.add(options, 'lineY').min(0).max(360).step(1);
+
+  var myPoint = gui.addFolder('point');
+  myPoint.add(options, 'pointCounts').min(1).max(10).step(1);
+  myPoint.add(options, 'pointStrokeWeight').min(1).max(100).step(1);
+
+  var myQuad = gui.addFolder('quad');
+  myQuad.add(options, 'quadX').min(0).max(100).step(1);
+  myQuad.add(options, 'quadY').min(0).max(100).step(1);
+
+  var myEllipse = gui.addFolder('ellipse');
+  myEllipse.add(options, 'ellipseX').min(0).max(100).step(1);
+  myEllipse.add(options, 'ellipseY').min(0).max(100).step(1);
+
+  var myTriangle = gui.addFolder('triangle');
+  myTriangle.add(options, 'triangleX2').min(0).max(100).step(1);
+  myTriangle.add(options, 'triangleY2').min(0).max(100).step(1);
+
+  var myRect = gui.addFolder('rect');
+  myRect.add(options, 'rectX').min(0).max(100).step(1);
+  myRect.add(options, 'rectY').min(0).max(100).step(1);
+
+
+};
--- _03-Patterns/03_02/sketch.js
+++ _03-Patterns/03_02/sketch.js
@@ -1,7 +1,7 @@
 // Global var
-var tileCount = 80;
 var max_distance = 50;
 var randomSeedValue = 0;
+var stateCounter = 0;
 
 
 function setup() {
@@ -21,22 +21,77 @@ function draw() {
   stroke(color(0), 0);
   strokeWeight(1);
 
-  for (columns = 0; columns < windowWidth*2; columns+=100) {
-    for (rows = 0; rows < windowHeight*2; rows+=100) {
+  for (columns = 0; columns < windowWidth * 2; columns += 100) {
+    for (rows = 0; rows < windowHeight * 2; rows += 100) {
       var diameter = dist(mouseX, mouseY, rows, columns);
-      diameter = diameter/max_distance*40;
+      diameter = diameter / max_distance * 40;
       push();
-      translate(rows, columns, diameter*5);
-      ellipse(0, 0, diameter, diameter);
-      //rect(0, 0, diameter, diameter); //try ellipse(...)
+      translate(rows, columns, diameter * 5);
+      switchState(diameter);
       pop();
     }
   }
 }
 
-function keyPressed() {
-  if (key == 's' || key == 'S') saveThumb(650, 350);
+function mousePressed() {
+
 }
+
+function switchStateLogic() {
+  if (stateCounter < 6) {
+    stateCounter++;
+  } else {
+    stateCounter = 0;
+  }
+}
+
+function switchState(diameter) {
+  switch (stateCounter) {
+    case 0:
+      arc(options.arcX,
+        options.arcY,
+        diameter,
+        diameter,
+        options.arcStartRadian,
+        options.arcEndRadian);
+      break;
+    case 1:
+      line(options.lineX,
+        options.lineY,
+        diameter,
+        diameter);
+      break;
+    case 2:
+      strokeWeight(options.pointStrokeWeight);
+      for (var i = 1; i <= options.pointCounts; i++) {
+        point(mouseX * i, mouseY * i);
+      }
+      break;
+    case 3:
+      quad(options.quadX, options.quadY, windowWidth - mouseX, 0, windowHeight - mouseY, 0, mouseX, mouseY);
+      break;
+    case 4:
+      ellipse(options.ellipseX, options.ellipseY, diameter, diameter);
+      break;
+    case 5:
+      triangle(diameter, diameter, options.triangleX2, options.triangleY2, mouseX, mouseY);
+      break;
+    case 6:
+      rect(options.rectX, options.rectY, diameter, diameter);
+      break;
+    default:
+      ellipse(options.ellipseX, options.ellipseY, diameter, diameter);
+  }
+}
+
+function keyPressed(event) {
+  if (key == 's' || key == 'S') {
+    saveThumb(650, 350);
+  } else if (event.which == 32) { //space
+    switchStateLogic();
+  }
+}
+
 // Tools
 
 // resize canvas when the window is resized
