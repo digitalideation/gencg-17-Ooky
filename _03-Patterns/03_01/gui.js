31e0a9996cfc44a2243254462df2bf1d1d38a5b9
Jony Margelist <jony.margelist@stud.hslu.ch>
2017-11-30 11:31:04 +0100

    fix alphaValue from GUI

--- _03-Patterns/03_01/gui.js
+++ _03-Patterns/03_01/gui.js
@@ -1,6 +1,6 @@
 var options = {
   tileCount: 20,
-  circleAlpha: 50,
+  backgroundAlphaValue: 50,
   circleColor: [47, 161, 214], //RGB
   enableMouseX: false,
 };
@@ -8,7 +8,7 @@ var options = {
 window.onload = function() {
   var gui = new dat.GUI();
   gui.add(options, 'tileCount').min(1).max(50).step(1);
-  gui.add(options, 'circleAlpha').min(0).max(255).step(1);
+  gui.add(options, 'backgroundAlphaValue').min(0).max(255).step(1);
   gui.add(options, 'enableMouseX');
   gui.addColor(options, 'circleColor');
 };
