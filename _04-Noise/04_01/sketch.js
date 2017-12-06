77cac0c2168bd460baf87670f69babadf5e804b8
Jony Margelist <jony.margelist@stud.hslu.ch>
2017-12-06 10:34:22 +0100

    first try

--- _04-Noise/04_01/sketch.js
+++ _04-Noise/04_01/sketch.js
@@ -1,5 +1,3 @@
-
-
 function setup() {
   // Canvas setup
   canvas = createCanvas(windowWidth, windowHeight);
@@ -10,7 +8,22 @@ function setup() {
 }
 
 function draw() {
+  background(0, 0, 0, 25);
+  noFill();
+  stroke(255);
+  beginShape();
+  var maxRange = map(mouseY, 0, height, 0, 5);
+  for (var x = 0; x < width; x++) {
+    var nx = millis() / 1000 * map(x, 0, width, 0, maxRange);
+    var y = height * noise(nx);
+    vertex(x, y);
+  }
+  endShape();
+}
 
+
+function keyPressed() {
+  if (key == 's' || key == 'S') saveThumb(650, 350);
 }
 
 
@@ -21,6 +34,6 @@ function windowResized() {
 
 // Thumb
 function saveThumb(w, h) {
-  let img = get( width/2-w/2, height/2-h/2, w, h);
-  save(img,'thumb.jpg');
+  let img = get(width / 2 - w / 2, height / 2 - h / 2, w, h);
+  save(img, 'thumb.jpg');
 }
