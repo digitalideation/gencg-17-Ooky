7227c4abee114b86782590b5bd3dce3621ab516c
Jony Margelist <jony.margelist@stud.hslu.ch>
2017-12-06 10:23:47 +0100

    setup empty project

---
+++ _04-Noise/04_01/sketch.js
@@ -0,0 +1,26 @@
+
+
+function setup() {
+  // Canvas setup
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("p5Container");
+  // Detect screen density (retina)
+  var density = displayDensity();
+  pixelDensity(density);
+}
+
+function draw() {
+
+}
+
+
+// resize canvas when the window is resized
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight, false);
+}
+
+// Thumb
+function saveThumb(w, h) {
+  let img = get( width/2-w/2, height/2-h/2, w, h);
+  save(img,'thumb.jpg');
+}
