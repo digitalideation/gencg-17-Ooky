a2b9778a9ac7be88751332a9f7c4363e67e11608
Jony Margelist <jony.margelist@stud.hslu.ch>
2017-12-13 11:02:10 +0100

    added mouseX and mouseY for R and G-value, also i check on the B value (so it never stops)

--- _05-Font/05_02/sketch.js
+++ _05-Font/05_02/sketch.js
@@ -15,40 +15,40 @@ function setup() {
   pixelDensity(density);
   background(50);
   myFont = "Barlow Semi Condensed";
-  myText = "ExampleText";
+  myText = "Digital Ideation";
   myTextWidth = textWidth(myText);
   myColor = color(255, 0, 0);
   myArray = [];
   textFont(myFont);
-  fill(255);
-  textSize(64);
-  text(myText, windowWidth / 2 - myTextWidth * 2, windowHeight / 2 - 64, 200, 200);
+  fill(0);
+  textSize(128);
+  text(myText, windowWidth / 2 - myTextWidth-800/2, windowHeight / 2 - 64, 800, 400);
   imageLocation = 0;
+}
 
+
+
+
+function draw() {
   loadPixels();
   changeColor();
   updatePixels();
 }
 
 function changeColor() {
+  let rValue = map(mouseX, 0, windowWidth, 0, 255);
+  let gValue = map(mouseY, 0, windowHeight, 0, 255);
   for (let x = 0; x < windowWidth; x++) {
     for (let y = 0; y < windowHeight; y++) {
-      imageLocation = (x + y * windowWidth)*4;
-      if(pixels[x*4+y*windowWidth*4] != 50) {
-        pixels[x * 4 + y * windowWidth * 4] = 30;    //r
-        pixels[x * 4 + y * windowWidth * 4+1] = 70;  //g
-        pixels[x * 4 + y * windowWidth * 4+2] = 110;  //b
+      imageLocation = (x + y * windowWidth) * 4;
+      if (pixels[x * 4 + y * windowWidth * 4+2] != 50) {
+        pixels[x * 4 + y * windowWidth * 4] = rValue; //r
+        pixels[x * 4 + y * windowWidth * 4 + 1] = gValue; //g
+        pixels[x * 4 + y * windowWidth * 4 + 2] = 0; //b
       }
     }
   }
 }
-
-
-function draw() {
-  //background(50);
-}
-
-
 
 
 function mousePressed() {
