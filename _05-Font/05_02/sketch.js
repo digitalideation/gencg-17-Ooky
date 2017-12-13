2b7b956e281ef9c4661792bdf8c326721ad098fb
Jony Margelist <jony.margelist@stud.hslu.ch>
2017-12-13 10:34:08 +0100

    loaded Pixels changed color, update Pixels

--- _05-Font/05_02/sketch.js
+++ _05-Font/05_02/sketch.js
@@ -1,6 +1,9 @@
 let myFont;
 let myText;
 let myTextWidth;
+let myColor;
+let imageLocation;
+let r, b, g;
 
 
 function setup() {
@@ -10,17 +13,39 @@ function setup() {
   // Detect screen density (retina)
   let density = displayDensity();
   pixelDensity(density);
-  myFont = "Barlow Semi Condensed";
-  myText = "SecondSampleProject";
-  myTextWidth = textWidth(myText);
-}
-
-function draw() {
   background(50);
+  myFont = "Barlow Semi Condensed";
+  myText = "ExampleText";
+  myTextWidth = textWidth(myText);
+  myColor = color(255, 0, 0);
+  myArray = [];
   textFont(myFont);
   fill(255);
   textSize(64);
-  text(myText, windowWidth/2-myTextWidth*2, windowHeight/2-64, 200, 200);
+  text(myText, windowWidth / 2 - myTextWidth * 2, windowHeight / 2 - 64, 200, 200);
+  imageLocation = 0;
+
+  loadPixels();
+  changeColor();
+  updatePixels();
+}
+
+function changeColor() {
+  for (let x = 0; x < windowWidth; x++) {
+    for (let y = 0; y < windowHeight; y++) {
+      imageLocation = (x + y * windowWidth)*4;
+      if(pixels[x*4+y*windowWidth*4] != 50) {
+        pixels[x * 4 + y * windowWidth * 4] = 30;    //r
+        pixels[x * 4 + y * windowWidth * 4+1] = 70;  //g
+        pixels[x * 4 + y * windowWidth * 4+2] = 110;  //b
+      }
+    }
+  }
+}
+
+
+function draw() {
+  //background(50);
 }
 
 
