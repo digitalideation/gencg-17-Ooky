03409e09c03e7260fe608c76ac36d9bb55e43884
Jony Margelist <jony.margelist@stud.hslu.ch>
2017-12-13 11:33:38 +0100

    setup project, empty logic for border outline

---
+++ _05-Font/05_03/sketch.js
@@ -0,0 +1,103 @@
+let myFont;
+let myText;
+let myTextWidth;
+let myColor;
+let imageLocation;
+let r, b, g;
+
+
+function setup() {
+  // Canvas setup
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("p5Container");
+  // Detect screen density (retina)
+  let density = displayDensity();
+  pixelDensity(density);
+  background(50);
+  myFont = "Barlow Semi Condensed";
+  myText = "Digital Ideation";
+  myTextWidth = textWidth(myText);
+  myColor = color(255, 0, 0);
+  myArray = [];
+  textFont(myFont);
+  fill(0);
+  textSize(128);
+  text(myText, windowWidth / 2 - myTextWidth - 200, windowHeight / 2 - 64, 800, 400);
+  imageLocation = 0;
+}
+
+
+
+
+function draw() {
+  loadPixels();
+  changeColor();
+  changeBorderColor();
+  updatePixels();
+}
+
+function changeBorderColor() {
+  getOutline();
+}
+
+function getOutline() {
+  for (let x = 0; x < windowWidth; x++) {
+    for (let y = 0; y < windowHeight; y++) {
+        //Check Top
+        //if()
+        //Check TopRight
+        //Check Right
+        //Check RightBottm
+        //Check Bottom
+        //Check BottomLeft
+        //Check Left
+        //Check LeftTop
+    }
+  }
+}
+  function changeColor() {
+    for (let x = 0; x < windowWidth; x++) {
+      for (let y = 0; y < windowHeight; y++) {
+        if (pixels[pixelLocation(x,y,"blue")] != 50) {//check for blue value
+          pixels[pixelLocation(x,y,"red")] = 255; //r
+          pixels[pixelLocation(x,y,"green")] = 255; //g
+          pixels[pixelLocation(x,y,"blue")] = 255; //b
+        }
+      }
+    }
+  }
+
+  function pixelLocation(x, y, colorValue) {
+    if(colorValue === "red") {
+      return (x * 4 + y * windowWidth * 4 + 0); //r
+    } else if (colorValue ==="green") {
+      return (x * 4 + y * windowWidth * 4 + 1); //g
+    } else if (colorValue ==="blue") {
+      return (x * 4 + y * windowWidth * 4 + 2); //b
+    } else {
+      return null;
+    }
+
+  }
+
+
+  function mousePressed() {
+
+  }
+
+
+  function keyPressed() {
+    if (key == 's' || key == 'S') saveThumb(650, 350);
+  }
+
+
+  // resize canvas when the window is resized
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight, false);
+  }
+
+  // Thumb
+  function saveThumb(w, h) {
+    let img = get(width / 2 - w / 2, height / 2 - h / 2, w, h);
+    save(img, 'thumb.jpg');
+  }
